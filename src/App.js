0fd0f094b3ee068c0b4da8ab2123e819b18bd745
rishav-raj1 <rishavkumarraj801@gmail.com>
2021-06-16 01:26:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+
+
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      setUsers(await response.json());
+
+  }
+  
+  useEffect(() => {
+          getUsers();
+  }, []);
+
+
+  return(
+    <div>
+
+  <h1 className="heading_style"> Rishav Kumar (Jsonplaceholder API) </h1>
+
+
+  {
+      users.map((curElem) => {
+          return (
+            <div>
+
+            <div className="cards">
+              <div className="card">
+              <h3 className="card_id">UserId:{curElem.userId}</h3>
+              <div className="card_info">
+              <span className="card_title">{curElem.title}</span>
+              <h3 className="card_id">id:{curElem.id}</h3>
+              </div>
+              </div>
+            </div>
+            </div>
+          )
+
+      })
+  }
+
+  </div>
+
   );
 }
 
-export default App;
+export default App
